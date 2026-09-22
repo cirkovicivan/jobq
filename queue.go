911b9fc48ded75bc96f5e38b402e16e3de985f33
cirko0 <cirko007ivan@gmail.com>
2026-09-22 16:36:19 +0200

    feat: add worker job consumption

--- queue.go
+++ queue.go
@@ -3,8 +3,15 @@ package main
 import "sync"
 
 type Queue struct {
-	mu   sync.Mutex
+	mu   *sync.Mutex
 	jobs []Job
+	cond *sync.Cond
+}
+
+func NewQueue() *Queue {
+	mu := sync.Mutex{}
+
+	return &Queue{mu: &mu, jobs: []Job{}, cond: sync.NewCond(&mu)}
 }
 
 func (q *Queue) Enqueue(job Job) {
@@ -12,14 +19,16 @@ func (q *Queue) Enqueue(job Job) {
 	defer q.mu.Unlock()
 
 	q.jobs = append(q.jobs, job)
+
+	q.cond.Signal()
 }
 
 func (q *Queue) Dequeue() (Job, bool) {
 	q.mu.Lock()
 	defer q.mu.Unlock()
 
-	if len(q.jobs) == 0 {
-		return Job{}, false
+	for len(q.jobs) == 0 {
+		q.cond.Wait()
 	}
 
 	job := q.jobs[0]
--- worker.go
+++ worker.go
@@ -1,1 +1,24 @@
 package main
+
+import "fmt"
+
+type Worker struct {
+	queue *Queue
+}
+
+func NewWorker(queue *Queue) *Worker {
+	w := &Worker{queue: queue}
+	return w
+}
+
+func (w *Worker) Start() {
+	for {
+		job, ok := w.queue.Dequeue()
+
+		if !ok {
+
+		}
+
+		fmt.Printf("%d", job.ID)
+	}
+}
